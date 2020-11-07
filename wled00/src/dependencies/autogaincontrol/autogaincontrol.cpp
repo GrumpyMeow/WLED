22f43794ad5a51cfb3d5e44e00afc476382c58ae
Sander Schutten <schutten@hotmail.com>
2020-11-07 14:24:11 +0100

    autogaincontrol added

---
+++ wled00/src/dependencies/autogaincontrol/autogaincontrol.cpp
@@ -0,0 +1,29 @@
+
+/* ----- LIBRARIES ----- */
+#include <Arduino.h>
+
+#include "autogaincontrol.h"
+
+
+// Constructor of autogaincontrol
+void autogaincontrol::init() {
+}
+
+int autogaincontrol::process(int micIn) {
+  // Using a ternary operator, the result is either 0 (when amplitude below soundSquelch (noise)) or it's a bit smoothed out with the previous amplitude.
+  int amplitude = abs(micIn);
+  int result = (amplitude <= soundSquelch) ? 0 : ((prevAmplitude * 3) + amplitude) / 4;
+
+  int sampleAdj = ((result * sampleGain) / 40) + (result / 16); // Adjust the gain.
+  prevAmplitude = min(sampleAdj, 255);;                         // We'll now make our rebase our sample to be adjusted.
+
+  sampleAvg = ((sampleAvg * 15) + sampleAdj) / 16;       			// Smooth it out over the last 16 samples.
+  
+  multAgc = (sampleAvg < 1) ? targetAgc : targetAgc / sampleAvg;// Make the multiplier so that sampleAvg * multiplier = setpoint
+
+  return (micIn * multAgc);
+}
+
+void autogaincontrol::print() {
+	//Serial.printf("%d %f %f %f\n", sample, sampleAvg, multAgc);
+}
---
+++ wled00/src/dependencies/autogaincontrol/autogaincontrol.h
@@ -0,0 +1,19 @@
+#ifndef autogaincontrol_h
+#define autogaincontrol_h
+
+class autogaincontrol {
+public:
+    void init();    
+    int process(int sample);
+    void print();
+private:
+    int sample;
+    int prevAmplitude;
+    float multAgc;                                      // sample * multAgc = sampleAgc. Our multiplier
+    uint8_t targetAgc = 60;                             // Target Auto Gain Control
+    byte sampleGain = 1;
+    float sampleAvg = 0;                                // Smoothed Average
+    byte soundSquelch = 10;
+};
+
+#endif
